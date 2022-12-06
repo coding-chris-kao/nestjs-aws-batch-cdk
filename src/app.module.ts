de8121ba737088449a9b1053e6e2bc6ef9b5834a
coding-chris-kao <chi1010860@gmail.com>
2022-12-06 18:26:42 +0800

    feat: remove winston

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { WinstonModule } from 'nest-winston';
 import { BasicCommand } from './basic/basic.command';
-import { winstonModuleFactory } from './common/winston.factory';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot(),
-    WinstonModule.forRootAsync({
-      useFactory: winstonModuleFactory,
-    }),
-  ],
+  imports: [ConfigModule.forRoot()],
   providers: [BasicCommand],
 })
 export class AppModule {}
